cf9fd58fe14f2e1f6175a65b13201b761cd521e8
John Smith <asionesjia@gmail.com>
2024-10-22 01:15:36 +0800

    :white_check_mark: 手机验证码登陆完成。

--- src/app/(login)/sign-up/_components/sign-up-sms.tsx
+++ src/app/(login)/sign-up/_components/sign-up-sms.tsx
@@ -5,11 +5,13 @@ import { Input } from '@/components/ui/input'
 import { useSearchParams } from 'next/navigation'
 import { useActionState, useEffect, useRef, useState } from 'react'
 import { ActionState } from '@/lib/auth/middleware'
-import { SendOtpAction, ValidateOtpAction } from '@/app/(login)/sign-up/actions'
+import { sendOtpAction } from '@/app/(login)/sign-up/actions'
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
 import { motion } from 'framer-motion'
 import ResendButton from '@/components/buttons/resend-button'
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { signUp } from '@/app/(login)/actions'
 
 type SignUpSmsProps = {}
 
@@ -20,35 +22,40 @@ const SignUpSms = ({}: SignUpSmsProps) => {
   const redirect = searchParams.get('redirect')
   const inviteId = searchParams.get('inviteId')
   const [sendOtpState, sendOtpFormAction, sendOtpPending] = useActionState<ActionState, FormData>(
-    SendOtpAction,
+    sendOtpAction,
     undefined,
   )
-  const [validateOtpState, validateOtpFormAction, validateOtpPending] = useActionState<
-    ActionState,
-    FormData
-  >(ValidateOtpAction, undefined)
+  const [signUpState, signUpFormAction, signUpPending] = useActionState<ActionState, FormData>(
+    signUp,
+    undefined,
+  )
 
   useEffect(() => {
-    if (otpCode?.length === 6 && !validateOtpPending) {
-      if (formRef.current && validateOtpState?.isOtpValidated === undefined) {
+    if (otpCode?.length === 6 && !signUpPending) {
+      if (formRef.current && signUpState?.isOtpValidated === '') {
         formRef.current.requestSubmit()
       }
     }
   }, [otpCode])
 
-  console.log(validateOtpState)
+  console.log(formRef?.current?.elements)
+
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <form
         ref={formRef}
         className="space-y-6"
-        action={sendOtpState?.otpToken ? validateOtpFormAction : sendOtpFormAction}
+        action={sendOtpState?.otpToken ? signUpFormAction : sendOtpFormAction}
       >
         <input type="hidden" name="redirect" value={redirect || ''} />
         <input type="hidden" name="inviteId" value={inviteId || ''} />
         <input type="hidden" name="otpToken" value={sendOtpState?.otpToken || ''} />
-        <input type="hidden" name="isOtpValidated" value={validateOtpState?.isOtpValidated || ''} />
-        <div>
+        <input type="hidden" name="isOtpValidated" value={signUpState?.isOtpValidated || ''} />
+        <div
+          className={
+            (signUpState?.isOtpValidated || signUpState?.isOtpValidated === undefined) && 'hidden'
+          }
+        >
           <Label htmlFor="phone">phone</Label>
           <div className="mt-1">
             <Input
@@ -66,7 +73,12 @@ const SignUpSms = ({}: SignUpSmsProps) => {
 
         {sendOtpState?.error && <div className="text-red-500 text-sm">{sendOtpState.error}</div>}
 
-        <div className="flex justify-between items-end">
+        <div
+          className={cn(
+            'flex justify-between items-end',
+            (signUpState?.isOtpValidated || signUpState?.isOtpValidated === 'true') && 'hidden',
+          )}
+        >
           {sendOtpState?.otpToken && (
             <motion.div
               className="flex flex-col"
@@ -116,15 +128,101 @@ const SignUpSms = ({}: SignUpSmsProps) => {
             />
           </motion.div>
         </div>
-        {validateOtpState?.error && (
-          <div className="text-red-500 text-sm">{validateOtpState.error}</div>
+        {signUpState?.error && <div className="text-red-500 text-sm">{signUpState.error}</div>}
+        {(signUpState?.isOtpValidated || signUpState?.isOtpValidated === 'true') && (
+          <>
+            <motion.div
+              initial={{ opacity: 0, x: -100, rotate: -10 }}
+              animate={{ opacity: 1, x: 0, rotate: 0 }}
+              exit={{}}
+              transition={{ type: 'spring', bounce: 0.4, duration: 0.8 }}
+            >
+              <Label htmlFor="username" className="text-nowrap">
+                Username
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  maxLength={50}
+                  placeholder="Enter your Username"
+                />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -100, rotate: -10 }}
+              animate={{ opacity: 1, x: 0, rotate: 0 }}
+              exit={{}}
+              transition={{ type: 'spring', bounce: 0.4, duration: 0.8, delay: 0.2 }}
+            >
+              <Label htmlFor="email" className="text-nowrap">
+                Email
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="email"
+                  name="email"
+                  type="text"
+                  autoComplete="email"
+                  required
+                  maxLength={50}
+                  placeholder="Enter your Email"
+                />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -100, rotate: -10 }}
+              animate={{ opacity: 1, x: 0, rotate: 0 }}
+              exit={{}}
+              transition={{ type: 'spring', bounce: 0.4, duration: 0.8, delay: 0.4 }}
+            >
+              <Label htmlFor="password" className="text-nowrap">
+                Password
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  maxLength={50}
+                  placeholder="Enter your password"
+                />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -100, rotate: -10 }}
+              animate={{ opacity: 1, x: 0, rotate: 0 }}
+              exit={{}}
+              transition={{ type: 'spring', bounce: 0.4, duration: 0.8, delay: 0.6 }}
+            >
+              <Label htmlFor="passwordConfirmation" className="text-nowrap">
+                Password Confirmation
+              </Label>
+              <div className="mt-1">
+                <Input
+                  id="passwordConfirmation"
+                  name="Password Confirmation"
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  maxLength={50}
+                  placeholder="Enter your Username"
+                />
+              </div>
+            </motion.div>
+          </>
         )}
         {sendOtpState?.otpToken && (
           <Button
             type="submit"
-            formAction={validateOtpFormAction}
+            formAction={signUpFormAction}
             className="w-full"
-            isLoading={validateOtpPending}
+            isLoading={signUpPending}
           >
             确定
           </Button>
