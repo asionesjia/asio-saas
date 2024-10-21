09f50fa4e484a7547da5084f15c5089e6c6d5896
John Smith <asionesjia@gmail.com>
2024-10-22 01:19:22 +0800

    :fire: 删除opt校验Action

--- src/app/(login)/sign-up/actions.ts
+++ src/app/(login)/sign-up/actions.ts
@@ -2,21 +2,12 @@
 
 import { validatedAction } from '@/lib/auth/middleware'
 import { z } from 'zod'
-import { decryptData, sendOtp } from '@/lib/otp/utils'
-
-const signInSmsSchema = z.object({
-  phone: z.string().regex(/^1[3-9]\d{9}$/),
-  email: z.string().email().min(3).max(255),
-  password: z.string().min(4).max(100),
-  otp: z.string().regex(/^\d{6}$/),
-  name: z.string().max(16),
-})
-export const SignUpSmsAction = validatedAction(signInSmsSchema, async (data, formData) => {})
+import { sendOtp } from '@/lib/otp/utils'
 
 const sendOtpSchema = z.object({
   phone: z.string().regex(/^1[3-9]\d{9}$/),
 })
-export const SendOtpAction = validatedAction(sendOtpSchema, async (data, formData) => {
+export const sendOtpAction = validatedAction(sendOtpSchema, async (data, formData) => {
   const { phone } = data
 
   try {
@@ -25,42 +16,5 @@ export const SendOtpAction = validatedAction(sendOtpSchema, async (data, formDat
     return { data: data, otpToken: token, otpTimedOutAt: timedOutAt, error: null }
   } catch (error) {
     return { data: data, otpToken: null, otpTimedOutAt: null, error: '发送失败。' }
-  }
-})
-
-const validateOtpSchema = z.object({
-  phone: z.string().regex(/^1[3-9]\d{9}$/),
-  otpCode: z
-    .string()
-    .regex(/^\d{6}$/)
-    .optional(),
-  otpToken: z.string().optional(),
-})
-export const ValidateOtpAction = validatedAction(validateOtpSchema, async (data, formData) => {
-  const { phone, otpCode, otpToken } = data
-
-  console.log(data)
-  try {
-    if (otpCode && otpToken) {
-      const dataStore = decryptData(otpToken)
-      if (dataStore.phone === phone && dataStore.otp === otpCode) {
-        if (dataStore.expiredAt < new Date(Date.now())) {
-          return {
-            data: data,
-            isOtpValidated: false,
-            error: '验证码已过期，请重新申请。',
-          }
-        }
-        return { data: data, isOtpValidated: true, error: null }
-      }
-      return {
-        data: data,
-        isOtpValidated: false,
-        error: '验证码错误。',
-      }
-    }
-    return { data: data, isOtpValidated: false, error: '请检查手机号或验证码是否正确。' }
-  } catch (error) {
-    return { data: data, isOtpValidated: false, error: '校验失败，请稍后重试。' }
   }
 })
